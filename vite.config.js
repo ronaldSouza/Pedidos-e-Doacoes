d9bb91adba26287c565d94c928b9fdd61146de45
Ronald Souza <telles.s@escolar.ifrn.edu.br>
2022-11-08 17:32:19 -0300

    Consertado erro aonde o vite não pre carregava os assets

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/assets/scss/argon-dashboard.scss',
+                'public/assets/js/core/popper.min.js',
+                'public/assets/js/core/bootstrap.min.js',
+                'public/assets/plugins/perfect-scrollbar.min.js',
+                'public/assets/plugins/smooth-scrollbar.min.js',
+                'public/assets/plugins/chartjs.min.js'
             ],
             refresh: true,
         }),
